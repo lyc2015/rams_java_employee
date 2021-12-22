6b27970dddd2f4feea36288e6e873f37c55ccc4e
wang <wangy@PJNTPB-JA01>
2021-12-22 15:45:52 +0900

    指摘対応

---
+++ src/main/java/jp/co/lyc/cms/mapper/SendInvoiceMapper.java
@@ -0,0 +1,42 @@
+package jp.co.lyc.cms.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import jp.co.lyc.cms.model.AccountInfoModel;
+import jp.co.lyc.cms.model.CostRegistrationModel;
+import jp.co.lyc.cms.model.ModelClass;
+import jp.co.lyc.cms.model.SendInvoiceModel;
+import jp.co.lyc.cms.model.SendInvoiceWorkTimeModel;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SendInvoiceMapper {
+	/**
+	 * 画面情報検索
+	 * 
+	 * @param TopCustomerNo
+	 * @return
+	 */
+
+	public List<SendInvoiceModel> selectSendInvoice(HashMap<String, String> dutyManagementModel);
+
+	public List<CostRegistrationModel> selectCostRegistration(HashMap<String, String> dutyManagementModel);
+
+	public List<SendInvoiceWorkTimeModel> selectSendInvoiceByCustomerNo(HashMap<String, String> dutyManagementModel);
+
+	public void insertInvoiceData(HashMap<String, String> model);
+
+	public void updateInvoiceData(HashMap<String, String> model);
+
+	public void insertNewInvoiceData(HashMap<String, String> model);
+
+	public void deleteInvoiceData(HashMap<String, String> model);
+
+	public void updateAllInvoiceData(HashMap<String, String> model);
+
+	public List<ModelClass> selectBankAccountInfo();
+
+	public AccountInfoModel getAccountInfo(String bankCode);
+}
---
+++ src/main/java/jp/co/lyc/cms/service/SendInvoiceService.java
@@ -0,0 +1,71 @@
+package jp.co.lyc.cms.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jp.co.lyc.cms.model.AccountInfoModel;
+import jp.co.lyc.cms.model.CostRegistrationModel;
+import jp.co.lyc.cms.model.ModelClass;
+import jp.co.lyc.cms.model.SendInvoiceModel;
+import jp.co.lyc.cms.model.SendInvoiceWorkTimeModel;
+import jp.co.lyc.cms.mapper.SendInvoiceMapper;
+
+@Component
+public class SendInvoiceService {
+
+	@Autowired
+	SendInvoiceMapper sendInvoiceMapper;
+
+	/**
+	 * 画面情報検索
+	 * 
+	 * @param TopCustomerNo
+	 * @return
+	 */
+
+	public List<SendInvoiceModel> selectSendInvoice(HashMap<String, String> dutyManagementModel) {
+		List<SendInvoiceModel> resultMod = sendInvoiceMapper.selectSendInvoice(dutyManagementModel);
+		return resultMod;
+	}
+
+	public List<CostRegistrationModel> selectCostRegistration(HashMap<String, String> dutyManagementModel) {
+		List<CostRegistrationModel> resultMod = sendInvoiceMapper.selectCostRegistration(dutyManagementModel);
+		return resultMod;
+	}
+
+	public List<SendInvoiceWorkTimeModel> selectSendInvoiceByCustomerNo(HashMap<String, String> dutyManagementModel) {
+		List<SendInvoiceWorkTimeModel> resultMod = sendInvoiceMapper.selectSendInvoiceByCustomerNo(dutyManagementModel);
+		return resultMod;
+	}
+
+	public void insertInvoiceData(HashMap<String, String> model) {
+		sendInvoiceMapper.insertInvoiceData(model);
+	}
+
+	public void updateInvoiceData(HashMap<String, String> model) {
+		sendInvoiceMapper.updateInvoiceData(model);
+	}
+
+	public void insertNewInvoiceData(HashMap<String, String> model) {
+		sendInvoiceMapper.insertNewInvoiceData(model);
+	}
+
+	public void deleteInvoiceData(HashMap<String, String> model) {
+		sendInvoiceMapper.deleteInvoiceData(model);
+	}
+
+	public void updateAllInvoiceData(HashMap<String, String> model) {
+		sendInvoiceMapper.updateAllInvoiceData(model);
+	}
+
+	public List<ModelClass> selectBankAccountInfo() {
+		return sendInvoiceMapper.selectBankAccountInfo();
+	}
+
+	public AccountInfoModel getAccountInfo(String bankCode) {
+		return sendInvoiceMapper.getAccountInfo(bankCode);
+	}
+}
